db6be613efbcf4e5328d10cb4fb19797c025a7ec
Nicholas Mead <nixter11@uw.edu>
2017-02-17 08:45:46 -0800

    fix name of tests and start class

--- Tests/WordTest.cs
+++ Tests/WordTest.cs
@@ -8,42 +8,42 @@ namespace WordCcounter
     public class WordTest
     {
         [Fact]
-        public void CountWord_CountWordInOneWordSentenceCaseSensitive_true()
+        public void CountWord_CountWordInOneLetterSentenceCaseSensitive_true()
         {
             RepeatCounter testWord = new RepeatCounter("h", "h");
             Assert.Equal(1, testWord.CountRepeats());
         }
 
         [Fact]
-        public void CountWord_CountWordInOneWordSentenceCaseSensitive_true()
+        public void CountWord_CountWordInMultiLetterSentenceCaseSensitive_true()
         {
             RepeatCounter testWord = new RepeatCounter("h", "h e l l h h");
             Assert.Equal(3, testWord.CountRepeats());
         }
 
         [Fact]
-        public void CountWord_CountWordInOneWordSentenceCaseSensitive_true()
+        public void CountWord_CountWordInMultiLetterSentenceCaseInSensitive_true()
         {
             RepeatCounter testWord = new RepeatCounter("h", "h e l l H H");
             Assert.Equal(3, testWord.CountRepeats());
         }
 
         [Fact]
-        public void CountWord_CountWordInOneWordSentenceCaseSensitive_true()
+        public void CountWord_CountWordInOneWordSentence_true()
         {
             RepeatCounter testWord = new RepeatCounter("hello", "Hello");
             Assert.Equal(1, testWord.CountRepeats());
         }
 
         [Fact]
-        public void CountWord_CountWordInOneWordSentenceCaseSensitive_true()
+        public void CountWord_CountWordInMultiWordSentence_true()
         {
             RepeatCounter testWord = new RepeatCounter("hello", "hello Hello dude");
             Assert.Equal(2, testWord.CountRepeats());
         }
 
         [Fact]
-        public void CountWord_CountWordInOneWordSentenceCaseSensitive_true()
+        public void CountWord_CountWordInMultiWordSentencePunctuationInsensitive_true()
         {
             RepeatCounter testWord = new RepeatCounter("hello", "hello, Hello, dude hello.");
             Assert.Equal(3, testWord.CountRepeats());
